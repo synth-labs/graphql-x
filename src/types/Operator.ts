b3409da787d90575e9b561f3c92c308aa5a5ebb1
Wyctus <ujszaszi.zzoltan@gmail.com>
2022-06-10 02:15:43 +0200

    feat: added IN and NOT IN operators

--- src/types/Operator.ts
+++ src/types/Operator.ts
@@ -1,13 +1,15 @@
 enum Operator {
-    EQUAL = '=',
-    NOT_EQUAL = '!=',
-    GREATER_THAN = '>',
-    GREATER_THAN_OR_EQUAL = '>=',
-    LESS_THAN = '<',
-    LESS_THAN_OR_EQUAL = '<=',
-    IS_NULL = 'IS NULL',
-    IS_NOT_NULL = 'IS NOT NULL',
-    LIKE = 'LIKE'
+    EQUAL = "=",
+    NOT_EQUAL = "!=",
+    GREATER_THAN = ">",
+    GREATER_THAN_OR_EQUAL = ">=",
+    LESS_THAN = "<",
+    LESS_THAN_OR_EQUAL = "<=",
+    IS_NULL = "IS NULL",
+    IS_NOT_NULL = "IS NOT NULL",
+    LIKE = "LIKE",
+    IN = "IN",
+    NOT_IN = "NOT IN"
 }
 
 export default Operator;
