a7af16e798babcbf8676232c4517a7edf882227a
Wyctus <ujszaszi.zzoltan@gmail.com>
2022-06-21 13:21:42 +0200

    feat: improved delete mutation error messages

--- src/builders/resolverGenerators/deleteTypeMutationResolver.ts
+++ src/builders/resolverGenerators/deleteTypeMutationResolver.ts
@@ -1,20 +1,17 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-import AnyMap from '../../types/AnyMap';
-import PrismaTable from '../../types/PrismaTable';
-import PrismaModel from '../../types/PrismaModel';
-import CustomError from '../../types/CustomError';
-
+import AnyMap from "../../types/AnyMap";
+import PrismaTable from "../../types/PrismaTable";
+import PrismaModel from "../../types/PrismaModel";
+import CustomError from "../../types/CustomError";
 
 function deleteTypeMutationResolver(tableName: string, prisma: PrismaClient) {
     return async (_parent: any, args: AnyMap) => {
         if (!args.id || !Number.isInteger(args.id)) {
-            throw new Error('The `id` argument is mandatory and must be an integer.');
+            throw new Error("The `id` argument is mandatory and must be an integer.");
         }
 
-
         if (!(tableName in prisma)) {
-
             throw new Error(`Tablename \`${tableName}\` is missing on Prisma client!`);
         }
 
@@ -29,10 +26,10 @@ function deleteTypeMutationResolver(tableName: string, prisma: PrismaClient) {
             });
         } catch (e) {
             const err: CustomError = <CustomError>e;
-            if (err.code === 'P2014') {
-                throw new Error('Cannot delete since the object has references!');
+            if (err.code === "P2014" || err.code === "P2003") {
+                throw new Error("Cannot delete since the object has references!");
             } else {
-                throw e;
+                throw new Error(err.message);
             }
         }
 
